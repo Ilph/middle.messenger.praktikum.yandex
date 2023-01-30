7c3303f8d9d08637329143ae33a851dbfeebb396
Ilphat <ilphat.gi@gmail.com>
2023-01-30 16:39:23 +0300

    fix errors

---
+++ server.cjs
@@ -0,0 +1,30 @@
+// import express from "express"
+// import { fileURLToPath } from "url"
+// import { join, dirname} from "path"
+
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = dirname(__filename)
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("./dist"))
+
+app.use("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./dist/index.html"))
+})
+
+function start() {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}`)
+    })
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+start()
